54204ed837030d3aa52abe83f14b3762287b1ee5
jeff kang <jeffyjkang@gmail.com>
2022-08-10 20:26:31 -0500

    update lecture stuff

---
+++ loops.js
@@ -0,0 +1,14 @@
+// const aNumber = 24
+
+// for (let x = 0; x <= aNumber; x++) {
+//   console.log(x)
+// }
+
+// // pseudo code
+
+// the highlighted element is at index 5
+
+
+// // for let (i = 0; i <= 15; i++){
+//   if element[i] is highlighted, show content on screen,
+// }
---
+++ pizza_maker.js
@@ -0,0 +1,44 @@
+// make conditions to check if it is a valid pizza
+// should have dough
+// should have sauce
+// should have cheese
+// should have toppings
+// should be backed/cooked
+
+// '00', 'bread', '
+
+// const crustType = 'thick_crust'
+// const sauce = 'marinara'
+// const cheese = 'moz'
+// const toppings = 'pepperoni'
+
+// if (crustType === 'thin_crust') {
+//   console.log('thin crust chosen')
+// }
+// else if (crustType === 'thick_crust') {
+//   console.log('thick crust chosen')
+// }
+
+// else (crustType !== 'thin_crust' && crustType !== 'thick_crust') {
+//   console.log('Crust error')
+// }
+
+// create a variable and assign it a number,
+// create a condition that checks the number,
+// console.log true if that number is less than or equal to zero,
+// else console.log false
+
+// lessThanOrEqualToZero(5) ➞ false
+
+// lessThanOrEqualToZero(0) ➞ true
+
+// lessThanOrEqualToZero(-2) ➞ true
+
+
+var num = -2
+if(num <= 0){
+console.log(`True`);
+}
+else{
+console.log(`False`);
+}
